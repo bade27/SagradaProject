dce664bcbe5be6aa53cd14c9de8d358c62702b5d
matteo <matteo.bade27@gmail.com>
2018-04-26 10:12:48 +0200

    dicebag test tmp

---
+++ src/test/java/Test/Model/DiceBagTest.java
@@ -0,0 +1,19 @@
+package Test.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceBagTest {
+    @org.junit.jupiter.api.BeforeAll
+    void setUp() {
+        DiceBag dicebag = new DiceBag();
+    }
+
+    @org.junit.jupiter.api.Test
+    void pickDices() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRemaining_dice() {
+    }
+
+}
