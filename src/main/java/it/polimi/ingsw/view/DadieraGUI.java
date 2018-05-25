1b2b5efde6603dba61a3edce8ddf1fbc6e7cdb17
CarafaF <federico.carafa@mail.polimi.it>
2018-05-25 13:38:52 +0200

    add updateDadiera

--- src/main/java/it/polimi/ingsw/view/DadieraGUI.java
+++ src/main/java/it/polimi/ingsw/view/DadieraGUI.java
@@ -4,33 +4,31 @@ import it.polimi.ingsw.model.ColorEnum;
 import it.polimi.ingsw.remoteInterface.Pair;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class DadieraGUI extends GridPane{
 
+    private GridPane pane;
     private int numPlayers;
     private int numOfDice;
     private String colore_dado_tolto;
     private String valore_dado_tolto;
-    private Pair[] pair;
     private boolean enable;
+    GridPane grid;
 
     public DadieraGUI(GridPane pane, int num) {
+        this.pane=pane;
         numPlayers = num;
         numOfDice = numPlayers * 2 + 1;
-        pair = new Pair[numOfDice];
+        Pair[] pair = new Pair[numOfDice];
         enable = false;
         for(Pair p : pair)
             p = new Pair(0, ColorEnum.WHITE);
-        GridPane grid = new GridPane();
-        for(int i=0;i< numOfDice;i++){
-            Button b=new Button(" ");
-            b.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-            grid.add(b, i, 0);
-        }
 
-        for (int i = 0; i < numOfDice; i++) {
+        updateGraphic(pair);
+        /*for (int i = 0; i < numOfDice; i++) {
             Button b = new Button("  ");
             b.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
             grid.add(b, i, 0);
@@ -49,8 +47,7 @@ public class DadieraGUI extends GridPane{
                 }
             });
 
-        }
-        pane.add(grid, 0, 1);
+        }*/
         dimWindows.dimHeight(grid, 20);
     }
     public String getColDadoTolto(){return colore_dado_tolto; }
@@ -60,4 +57,28 @@ public class DadieraGUI extends GridPane{
     public void setColDadoTolto(){colore_dado_tolto=null; }
 
     public void setValDadoTolto(){valore_dado_tolto=null; }
+
+    public void updateGraphic(Pair[] p){
+        grid = new GridPane();
+        for (int i = 0; i < p.length; i++) {
+            Button b = new Button("  ");
+            b.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            grid.add(b, i, 0);
+            if(p[i].getValue() != 0)
+                b.setText(""+p[i].getValue());                         //setta numero correttamente
+            b.setStyle("-fx-background-color: " + p[i].getColor());
+            b.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if(enable&&colore_dado_tolto==null&&valore_dado_tolto==null) {
+                        colore_dado_tolto = b.getStyle();
+                        valore_dado_tolto = b.getText();
+                        b.setText(null);
+                        b.setStyle(null);
+                    }
+                }
+            });
+        }
+        pane.add(grid, 0, 1);
+    }
 }
