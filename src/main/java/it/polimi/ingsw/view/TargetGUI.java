f9293f290ae12967fcd0988bc8df16bff10becec
CarafaF <federico.carafa@mail.polimi.it>
2018-06-17 22:47:33 +0200

    fix updatePublicTarget in targetGUI 2

--- src/main/java/it/polimi/ingsw/view/TargetGUI.java
+++ src/main/java/it/polimi/ingsw/view/TargetGUI.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.GUI;
+import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
@@ -43,28 +44,31 @@ public class TargetGUI extends GridPane{
 
 
     public void updatePrivateTarget(String []s){
-
-        String [] vecname0=s[0].split("\\/");
-        String name0=(vecname0[vecname0.length-1].split("\\."))[0];
-        bprivate.setText(name0);
-        bprivate.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
+        Platform.runLater(() -> {
+            String[] vecname0 = s[0].split("\\/");
+            String name0 = (vecname0[vecname0.length - 1].split("\\."))[0];
+            bprivate.setText(name0);
+            bprivate.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        });
     }
 
     public void updatePublicTarget(String []s){
-        String [] vecname1=s[0].split("\\/");
-        String name1=(vecname1[vecname1.length-1].split("\\."))[0];
-        bpublic1.setText(name1);
-        bpublic1.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
+        Platform.runLater(() -> {
+            String[] vecname1 = s[0].split("\\/");
+            String name1 = (vecname1[vecname1.length - 1].split("\\."))[0];
+            bpublic1.setText(name1);
+            bpublic1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
-        String [] vecname2=s[1].split("\\/");
-        String name2=(vecname2[vecname2.length-1].split("\\."))[0];
-        bpublic2.setText(name2);
-        bpublic2.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
+            String[] vecname2 = s[1].split("\\/");
+            String name2 = (vecname2[vecname2.length - 1].split("\\."))[0];
+            bpublic2.setText(name2);
+            bpublic2.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
-        String [] vecname3=s[02].split("\\/");
-        String name3=(vecname3[vecname3.length-1].split("\\."))[0];
-        bpublic3.setText(name3);
-        bpublic3.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
+            String[] vecname3 = s[02].split("\\/");
+            String name3 = (vecname3[vecname3.length - 1].split("\\."))[0];
+            bpublic3.setText(name3);
+            bpublic3.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        });
     }
 
 }
