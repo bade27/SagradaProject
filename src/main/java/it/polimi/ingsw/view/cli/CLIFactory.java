7ec9fe69e4dfb82c1767e928fe32bb5b2202873b
CarafaF <federico.carafa@mail.polimi.it>
2018-07-04 18:17:46 +0200

    add two new windows and ad doc in CLI factory

--- src/main/java/it/polimi/ingsw/view/cli/CLIFactory.java
+++ src/main/java/it/polimi/ingsw/view/cli/CLIFactory.java
@@ -9,6 +9,13 @@ import java.io.File;
 
 public class CLIFactory {
 
+    /**
+     *
+     * @param path XML file location
+     * @param name tool's name
+     * @return tool's number
+     * @throws ParserXMLException
+     */
     public static int getToolnumberFromName (String path , String name) throws ParserXMLException
     {
         try
@@ -35,6 +42,13 @@ public class CLIFactory {
         }
     }
 
+    /**
+     *
+     * @param path XML file location
+     * @param name tool's name
+     * @return tool's effect
+     * @throws ParserXMLException
+     */
     public  static String getTooldescriptionFromName(String path , String name) throws ParserXMLException{
         try{
             File file = new File(path);
@@ -49,7 +63,7 @@ public class CLIFactory {
                 String s = "tool" + (i + 1);
                 int numtool = Integer.parseInt(document.getElementsByTagName(s).item(0).getAttributes().getNamedItem("num").getTextContent());
                 String nm = document.getElementsByTagName("name").item(numtool-1).getTextContent();
-                String desTool = document.getElementsByTagName(s).item(0).getAttributes().getNamedItem("effect").getTextContent();
+                String desTool = document.getElementsByTagName("effect").item(i).getTextContent();
                 if (nm.equals(name))
                     return desTool;//n/d andrà sostiutito dal path immagine
             }
@@ -60,28 +74,4 @@ public class CLIFactory {
         }
     }
 
-    public  static String getToolnameFromName(String path , String name) throws ParserXMLException{
-        try{
-            File file = new File(path);
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(file);
-
-            int nCard = Integer.parseInt(document.getElementsByTagName("numberOfCards").item(0).getTextContent());
-
-            for (int i = 0; i < nCard;i++)
-            {
-                String s = "tool" + (i + 1);
-                int numtool = Integer.parseInt(document.getElementsByTagName(s).item(0).getAttributes().getNamedItem("num").getTextContent());
-                String nm = document.getElementsByTagName("name").item(numtool-1).getTextContent();
-                String desTool = document.getElementsByTagName(s).item(0).getAttributes().getNamedItem("effect").getTextContent();
-                if (nm.equals(name))
-                    return desTool;//n/d andrà sostiutito dal path immagine
-            }
-            throw new ParserXMLException("Name: " + name + " Not founded in path: " + path);
-        }
-        catch (Exception ex){
-            throw new ParserXMLException("Impossible to read file: " + path);
-        }
-    }
 }
