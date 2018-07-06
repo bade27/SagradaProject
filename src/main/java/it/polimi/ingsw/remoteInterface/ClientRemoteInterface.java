dc96f11326a8aeb82f395a7aa0a9cbbf9af647b7
AndreaBiondaPolimi <andrea.bionda@mail.polimi.it>
2018-07-06 23:24:30 +0200

    Some docs added

--- src/main/java/it/polimi/ingsw/remoteInterface/ClientRemoteInterface.java
+++ src/main/java/it/polimi/ingsw/remoteInterface/ClientRemoteInterface.java
@@ -9,19 +9,59 @@ import java.util.ArrayList;
 public interface ClientRemoteInterface extends Remote
 {
     //Utilities
+    /**
+     * Ping methods for testing if client is alive
+     * @return true if client respond, false otherwise
+     */
     public boolean ping () throws RemoteException;
+
+    /**
+     * Send a message to client
+     * @param s message
+     * @return true if client respond, false otherwise
+     */
     public boolean sendMessage (String s) throws ClientOutOfReachException,RemoteException;
+
+    /**
+     * Close communication to client from server
+     * @param cause cause of closing
+     * @return true if client respond, false otherwise
+     */
     public boolean closeCommunication (String cause) throws ClientOutOfReachException, RemoteException;
 
+
     //Setup
+
+    /**
+     * Ask to client username
+     * @return client's username
+     */
     public String login() throws ClientOutOfReachException, RemoteException;
+
+    /**
+     * Ask to client which pattern card he want to use
+     * @param s1 card number one
+     * @param s2 card number two
+     * @return pattern card selected
+     */
     public String chooseWindow(String[] s1, String[] s2)  throws ClientOutOfReachException, RemoteException;
+
+    /**
+     * Send to client all information about his tools, public and private objectives
+     * @param s card
+     * @return true if client respond, false otherwise
+     */
     public boolean sendCards(String[]... s) throws ClientOutOfReachException, RemoteException;
 
     //Game
+
+    /**
+     * Communicate to client about his turn
+     * @return Client response
+     */
     public String doTurn () throws ClientOutOfReachException,RemoteException;
 
-    //Game updates
+    //Update
     public String updateGraphic(Pair[] dadiera) throws ClientOutOfReachException,RemoteException;
     public String updateGraphic(Pair[][] grid) throws ClientOutOfReachException,RemoteException;
     public String updateOpponents(String user,Pair[][] grid,boolean active) throws ClientOutOfReachException,RemoteException;
@@ -29,6 +69,12 @@ public interface ClientRemoteInterface extends Remote
     public String updateRoundTrace(ArrayList<Pair>[] dice) throws RemoteException,ClientOutOfReachException;
 
     //End Game
+
+    /**
+     * Send result of match at the end of the match
+     * @param u list of players
+     * @param p list of points
+     */
     public void sendResults (String[] u,int [] p) throws RemoteException,ClientOutOfReachException;
 
     //Reconnection
--- src/main/java/it/polimi/ingsw/remoteInterface/ServerRemoteInterface.java
+++ src/main/java/it/polimi/ingsw/remoteInterface/ServerRemoteInterface.java
@@ -8,27 +8,47 @@ import java.rmi.RemoteException;
 public interface ServerRemoteInterface extends Remote
 {
     //Connection Phase
+
+    /**
+     * Set interface on server (used in rmi to communicate to server client's stub)
+     * @param client client interface
+     */
     public void setClient (ClientRemoteInterface client) throws RemoteException;
 
     //Game Phase
+
+    /**
+     * Communicate to server user's move
+     * @param coord coordinates about place
+     * @param pair die placed
+     * @return Result of move
+     */
     public String makeMove(Coordinates coord, Pair pair) throws RemoteException;
+
+    /**
+     * Ask to server if client's can use tool passed
+     * @param nrTool number of tool that client's want to use
+     * @return result of asking
+     */
     public String askToolPermission(int nrTool) throws RemoteException;
+
+    /**
+     * Communicate to server that user want to pass turn
+     * @return server response
+     */
     public String passTurn() throws RemoteException;
 
 
-    //Tool nr. 1-6-7-10-11
+    //Many overloads about using tool, because every tool use different resources
     public String useTool (Pair p, String instruction) throws RemoteException;
-    //Tool nr. 2-3
     public String useTool (Coordinates sartCoord,Coordinates endCoord) throws RemoteException;
-    //Tool nr.4
     public String useTool (Pair p ,Coordinates sartCoord1,Coordinates endCoord1, Coordinates sartCoord2,Coordinates endCoord2) throws RemoteException;
-    //Tool nr.5
     public String useTool (Pair dadiera,Pair trace,int nrRound) throws RemoteException;
-    //Tool nr.8
     public String useTool () throws RemoteException;
-    //Tool nr.9
     public String useTool (Pair p , Coordinates endCoord) throws RemoteException;
 
-    //Disconnection message
+    /**
+     * Communicate to server that client has closed window
+     */
     public void disconnection () throws  RemoteException;
 }
